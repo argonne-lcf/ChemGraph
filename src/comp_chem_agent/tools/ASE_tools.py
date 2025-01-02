68292637f113ad87d49a591fcfb521e7e2e7a033
tdpham2 <phamducthang97@gmail.com>
2025-01-02 10:00:56 -0600

    Add molecule_name_to_smiles tool

--- src/comp_chem_agent/tools/ASE_tools.py
+++ src/comp_chem_agent/tools/ASE_tools.py
@@ -1,45 +1,20 @@
 from langchain_core.tools import tool
 from typing import List, Dict, Any, Optional, Annotated
 from comp_chem_agent.models.atomsdata import AtomsData
+import pubchempy
 
 @tool
-def smiles_to_xyz(smiles: str):
+def molecule_name_to_smiles(name: str) -> str:
     """
-    Convert a SMILES string to a.
+    Convert a molecule name SMILES format.
     
     Args:
-        smiles: Input SMILES string.
+        name: Molecule name.
 
     Returns:
-        coords: coordinates of the atoms.
+        SMILES string.
     """
-    from rdkit import Chem
-    from rdkit.Chem import AllChem
-
-    # Generate the molecule object
-    mol = Chem.MolFromSmiles(smiles)
-    if mol is None:
-        raise ValueError("Invalid SMILES string.")
-    
-    # Add hydrogens and optimize 3D structure
-    mol = Chem.AddHs(mol)
-    if AllChem.EmbedMolecule(mol) != 0:
-        raise ValueError("Failed to generate 3D coordinates.")
-    if AllChem.UFFOptimizeMolecule(mol) != 0:
-        raise ValueError("Failed to optimize 3D geometry.")
-    
-    # Extract atomic information
-    conf = mol.GetConformer()
-    atoms = mol.GetAtoms()
-
-    coords = ''        
-    for ida, atom in enumerate(atoms):
-        pos = conf.GetAtomPosition(atom.GetIdx())
-        coords = coords + f"{atom.GetSymbol()} {pos.x:.6f} {pos.y:.6f} {pos.z:.6f}"
-        if ida < len(atoms):
-            coords += ";"
-        #xyz_file.write(f"{atom.GetSymbol()} {pos.x:.6f} {pos.y:.6f} {pos.z:.6f}\n")
-    return coords
+    return pubchempy.get_compounds(str(name), "name")[0].canonical_smiles
 
 @tool
 def smiles_to_atomsdata(smiles: str) -> AtomsData:
@@ -83,39 +58,6 @@ def smiles_to_atomsdata(smiles: str) -> AtomsData:
     )
     #print(type(atoms_data))
     return atoms_data
-
-@tool
-def geometry_optimization(coords: str):
-    """
-    Run geometry optimization using PySCF by reading in the coordinates.
-    
-    Args:
-        coords: coordinates of the atoms.
-
-    Returns:
-        optimized_structure: coordinates of the atoms after geometry optimization
-    """
-    from pyscf import gto, scf, geomopt
-    from pyscf.geomopt.berny_solver import optimize
-
-    mol = gto.M(
-        atom=coords,
-        basis='6-31g',
-        charge=0,
-        spin=0,
-    )
-    
-    # Run geometry optimization
-    mf = scf.RHF(mol)
-    optimized_mol = optimize(mf)
-    print("\nOptimized Geometry (Angstrom):")
-    for atom in optimized_mol.atom:
-        print(atom)
-    
-    # Print the total energy of the optimized geometry
-    print("\nTotal Energy (Hartree):", mf.e_tot)
-
-    return optimized_mol.atom
 
 @tool
 def geometry_optimization_ase(atomsdata: AtomsData) -> AtomsData:
