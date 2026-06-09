42b15a02e477398b64bd07444ad6163458219315
Jinchu Li <jinchuli2002@gmail.com>
2026-06-09 13:28:00 -0500

    Read version from chemgraphagent distribution

--- src/chemgraph/__init__.py
+++ src/chemgraph/__init__.py
@@ -1,11 +1,10 @@
  
 """ChemGraph package metadata."""
 
-from importlib.metadata import PackageNotFoundError, packages_distributions, version
+from importlib.metadata import PackageNotFoundError, version
 
 try:
-    dist_names = packages_distributions().get("chemgraph", [])
-    __version__ = version(dist_names[0]) if dist_names else "unknown"
-except (PackageNotFoundError, IndexError):
+    __version__ = version("chemgraphagent")
+except PackageNotFoundError:
     # Local source tree without installed package metadata.
     __version__ = "unknown"
