e8d259e81555c29a82fbfea73d34b3cd36b3674d
Thang Pham <tpham@anl.gov>
2026-07-01 15:33:52 -0500

    Remove evaluation files from main

--- scripts/evaluations/legacy_comm_chem_paper/generate_evaluation_data/Exp12/find_error.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-
-with open(sys.argv[1], "r") as rf:
-    data = json.load(rf)
-
-for item in data:
-    if "ERROR" in data[item]["manual_workflow"]["result"]['value']:
-        print(item)
